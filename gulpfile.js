0ea5ddfb9211731a16a3b52864d1485244fbc357
Myrkotyn <prohorovychua@gmail.com>
2016-06-19 12:38:27 +0300

    Add imagemin, comments

--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,13 @@ var browserSync = require("browser-sync");
 var reload = browserSync.reload;
 var autoprefix = require("gulp-autoprefixer");
 var uglify = require("gulp-uglify");
+var imagemin = require('gulp-imagemin');
 var source = {
     scssAll: "app/src/styles/**/*.scss",
     scss: "app/src/styles/main.scss",
     html: "app/src/*.html",
     htmlAll: "app/src/**/*.html",
-    image: "app/src/img/*",
+    image: "app/src/img/*.{jpg,jpeg,png,gif}",
     font: "app/src/font/*",
     jsFolder: "app/src/js/*.js"
 }
@@ -23,30 +24,53 @@ var dist = {
     jsFolder: "app/dist/js"
 }
 
+//
+//taks for all style
+//compile scss to css
+//autoprefix
+//
 gulp.task("css", function() {
-    gulp.src(source.scss).pipe(sass().on('error', sass.logError)).pipe(autoprefix("last 3 version")).pipe(gulp.dest(dist.css)).pipe(reload({
+    gulp.src(source.scss).pipe(sass().on('error', sass.logError)).pipe(autoprefix({
+        browsers: 'last 3 version',
+        cascade: false
+    })).pipe(gulp.dest(dist.css)).pipe(reload({
         stream: true
-    }))
+    }));
 })
+//
+//task for html
+//rigger
+//
 gulp.task("html:build", function() {
     gulp.src(source.html).pipe(rigger()).pipe(gulp.dest(dist.html)).pipe(reload({
         stream: true
     }))
 })
-
+//
+//task for js
+//uglify
+//
 gulp.task("js", function() {
     gulp.src(source.jsFolder).pipe(uglify()).pipe(gulp.dest(dist.jsFolder));
 })
-
-
+//
+//task for images
+//imagemin
+//
 gulp.task("image:build", function() {
-    gulp.src(source.image).pipe(gulp.dest(dist.image))
+    gulp.src(source.image)
+    .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+    .pipe(gulp.dest(dist.image))
 })
-
+//
+//task for fonts
+//
 gulp.task("font:build", function() {
     gulp.src(source.font).pipe(gulp.dest(dist.font))
 })
-
+//
+//task for combine all fron tasks in one and watch for them
+//
 gulp.task("serve", function() {
     browserSync({
         server: dist.html
@@ -55,5 +79,4 @@ gulp.task("serve", function() {
     gulp.watch(source.jsFolder, ["js"]);
     gulp.watch(source.htmlAll, ["html:build"]).on("change", reload);
 })
-
-gulp.task("default", ["serve", "css", "html:build", "font:build", "image:build", "js"], function(){} );
+gulp.task("default", ["serve", "css", "html:build", "font:build", "image:build", "js"], function() {});
